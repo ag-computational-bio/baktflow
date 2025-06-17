e8fdd50adc9bb191ce6a7b48fd26ab758bf9f3fa
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2025-06-17 10:42:30 +0200

    bump bakta to v1.11.0

--- setup-conda.sh
+++ setup-conda.sh
@@ -18,7 +18,7 @@ mamba create --yes --quiet -p conda/checkm2 checkm2=1.0.1
 mamba create --yes --quiet -p conda/mash mash=2.3
 mamba create --yes --quiet -p conda/gtdbtk gtdbtk=2.3.2
 mamba create --yes --quiet -p conda/ska ska=1.0
-mamba create --yes --quiet -p conda/bakta bakta=1.10.3 ncbi-amrfinderplus=4.0.3
+mamba create --yes --quiet -p conda/bakta bakta=1.11.0 ncbi-amrfinderplus=4.0.3
 mamba create --yes --quiet -p conda/tax-16-s blast=2.12.0
 mamba create --yes --quiet -p conda/vf diamond=2.0.14
 mamba create --yes --quiet -p conda/amrfinderplus ncbi-amrfinderplus=4.0.3
