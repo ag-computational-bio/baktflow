1ad133ab42671019328f082f1d5f6f2d8c3b283a
Julian Hahnfeld <84308019+jhahnfeld@users.noreply.github.com>
2026-03-02 13:45:27 +0100

    Rewrite CLI setup

--- baktflow/nextflow.py
+++ baktflow/nextflow.py
@@ -1,6 +1,7 @@
 import subprocess
 import logging
 import os
+import shutil
 from pathlib import Path
 
 logger = logging.getLogger(__name__)
@@ -9,47 +10,36 @@ c_green = "\033[1;32m"
 c_reset = "\033[0m"
 
 
-def start(setup_script, setup_dir, conda_dir, database_dir, nextflow_path=None):
+def baktflow_setup(setup_script: Path, setup_dir: Path, conda_dir: Path, database_dir: Path, conda_implementation: str, nextflow_path: str | None):
     """Run Nextflow setup script."""
-    
-    # Use a default nextflow_path if not provided
     if nextflow_path is None:
-        nextflow_path = os.environ.get('NEXTFLOW_HOME', '')  # Environment variable for Nextflow path
-    
-    # Use an alternative default path if NEXTFLOW_HOME is not set
-    if not nextflow_path:
-        nextflow_path = os.path.expanduser("~/nextflow")  # Default path in user's home directory
-    
-    # Validate the nextflow_path
+        nextflow_path = shutil.which('nextflow')
+        if not bool(nextflow_path):
+            raise 'Could not find nextflow executable. Please provide the path to the executable with: "--nextflow_path"'
     if not os.path.exists(nextflow_path):
         logger.error(f"Nextflow not found at {nextflow_path}. Please provide a valid path.")
-        return
+        raise f'Could not find nextflow executable at {nextflow_path}. Please provide a valid path.'
+
+    if not os.path.exists(setup_script):
+        logger.error(f"Cannot find script file: {setup_script}")
+        raise f'Could not find nextflow setup script at {setup_script}'
     
-    root_path = Path(__file__).resolve().parent.parent 
-            
-    # Dynamically determine the path to the setup.nf script
-    setup_script_path = os.path.join(root_path, 'nextflow', setup_script)  # Pointing to 'nextflow/setup.nf'
-    
-    if not os.path.exists(setup_script_path):
-        logger.error(f"Cannot find script file: {setup_script_path}")
-        return
-    
-    # Construct the Nextflow command
-    nextflow_cmd = f"{nextflow_path} run {setup_script_path} -profile standard"
+    nextflow_cmd: str = f"{nextflow_path} run {setup_script} -profile standard --cacheDir {conda_dir} --databaseDir {database_dir}"
+    if conda_implementation == 'micromamba':
+        nextflow_cmd += ' --useMicromamba true'
+    elif conda_implementation == 'mamba':
+        nextflow_cmd += ' --useMamba true'
+
+    nextflow_clean_cmd: str = f"{nextflow_path} clean -f -q"
 
     try:
-        # Set up Conda and database directories
         env = os.environ.copy()
-        env['CONDA_ENVS_PATH'] = str(conda_dir)
-        env['DATABASE_DIR'] = str(database_dir)
-
-        # Execute the Nextflow command
         subprocess.run(nextflow_cmd, check=True, shell=True, cwd=str(setup_dir), env=env)
-        
+        subprocess.run(nextflow_clean_cmd, check=True, shell=True, cwd=str(setup_dir), env=env)
+        shutil.rmtree(setup_dir.joinpath('work'), ignore_errors=True)
     except subprocess.CalledProcessError as e:
         logger.error(f"Nextflow setup script failed: {e}")
-        raise
-
+        raise e
 
 
 
